ec4a66f9ced477b177d1986dc5f8179f739246ef
Vladisalav Cheparin <v.cheparin@infotech.team>
2020-05-23 00:28:26 +0300

    k-near

--- src/main/java/deep/Students.java
+++ src/main/java/deep/Students.java
@@ -6,7 +6,6 @@ import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.BatchNormalization;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
@@ -18,10 +17,10 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.KFoldIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
 import org.nd4j.linalg.learning.config.Adam;
 import org.nd4j.linalg.learning.config.IUpdater;
-import org.nd4j.linalg.learning.config.Nadam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,9 +55,7 @@ public class Students {
         log.info("Calculated metrics");
         log.info("Min: {}", preProcessor.getMin());
         log.info("Max: {}", preProcessor.getMax());
-
-        //iterator.setPreProcessor(preProcessor);
-
+        iterator.setPreProcessor(preProcessor);
         final DataSet data = iterator.next();
         data.shuffle();
         data.shuffle();
@@ -73,10 +70,9 @@ public class Students {
         for (IUpdater iUpdater : iUpdaters) {
             System.out.println("Started updater " + iUpdater.getClass().getSimpleName());
             for (LossFunctions.LossFunction value : allLossFuncs()) {
-                trainAndEvalModel(labelIndex, numClasses, 10000, value, iUpdater, split);
+                trainAndEvalModel(labelIndex, numClasses, 3000, value, iUpdater, data);
                 System.out.println(value);
             }
-
             System.out.println("Finished updater " + iUpdater.getClass().getSimpleName());
             if (iUpdater instanceof Adam) {
                 System.out.println("Learn rate: " + ((Adam) iUpdater).getLearningRate());
@@ -87,53 +83,52 @@ public class Students {
 
     public static void trainAndEvalModel(int in, int out, int epochs,
                                          LossFunctions.LossFunction lossFunc,
-                                         IUpdater iUpdater,
-                                         SplitTestAndTrain split) {
+                                         IUpdater iUpdater, DataSet ds) {
         final double[] means = {0.33, 0.40, 0.32, 0.026, 0.013, 0.0027, 0.017, 0.04};
         final double[] std = {0.47, 0.28, 0.25, 0.03, 0.02, 0.015, 0.046, 0.051};
 
         final MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .activation(Activation.HARDSIGMOID)
+                .activation(Activation.SIGMOID)
                 .weightInit(WeightInit.XAVIER)
                 .updater(iUpdater)
-                //.l1(1e-4)
-                // .weightDecay(0.001)
-                //.l2(1e-3)
+                .l1(1e-4)
+                .weightDecay(1e-3)
+                //.l2(1e-4)
                 .list()
-                //                .layer(new IndicatorLayer.Builder().nIn(labelIndex)
-                //                        .nOut(4)
-                //                        .indWeight(new double[]{0.05, 0.05, 0.05, 0.05, 0.05, 2, 2, 2})
-                //                        .build())
-                //                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
-                //                        .nIn(4).nOut(numClasses).build())
+                //.layer(new IndicatorLayer.Builder().nIn(labelIndex)
+                //        .nOut(4)
+                //        .indWeight(new double[]{0.05, 0.05, 0.05, 0.05, 0.05, 2, 2, 2})
+                //        .build())
+                //.layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                //        .nIn(4).nOut(numClasses).build())
 
-//                .layer(new DenseLayer.Builder().nIn(in)
-//                                               .nOut(210)
-//                                               //  .dropOut(0.5)
-//                                               //.indWeight(std)
-//                                               .build())
-//                .layer(new DenseLayer.Builder().nIn(210)
-//                                               .nOut(100)
-//                                               // .dropOut(0.5)
-//                                               .build())
-//                .layer(new DenseLayer.Builder().nIn(100)
-//                                               .nOut(15)
-//                                               //   .dropOut(0.5)
-//                                               .build())
+                //.layer(new DenseLayer.Builder().nIn(in)
+                //                               .nOut(210)
+                //                               //  .dropOut(0.5)
+                //                               //.indWeight(std)
+                //                               .build())
+                //.layer(new DenseLayer.Builder().nIn(210)
+                //                               .nOut(100)
+                //                               // .dropOut(0.5)
+                //                               .build())
+                //.layer(new DenseLayer.Builder().nIn(100)
+                //                               .nOut(15)
+                //                               //   .dropOut(0.5)
+                //                               .build())
                 //.layer(new BatchNormalization())
-                //                .layer(new DenseLayer.Builder().nIn(10)
-                //                                               .nOut(210)
-                //                                               .build())
+                //.layer(new DenseLayer.Builder().nIn(10)
+                //                               .nOut(210)
+                //                               .build())
                 .layer(new DenseLayer.Builder().nIn(in)
-                                               .nOut(7)
+                                               .nOut(15)
                                                //.dropOut(0.5)
                                                .build())
-                .layer(new BatchNormalization())
-                .layer(new DenseLayer.Builder().nIn(7)
+               // .layer(new BatchNormalization())
+                .layer(new DenseLayer.Builder().nIn(15)
                                                .nOut(5)
-                                               //.dropOut(0.5)
+                                              // .dropOut(0.5)
                                                .build())
-                .layer(new BatchNormalization())
+               // .layer(new BatchNormalization())
                 .layer(new OutputLayer.Builder(lossFunc)
                         .nIn(5).nOut(out).build())
                 .build();
@@ -142,14 +137,25 @@ public class Students {
 
         model.init();
         model.setListeners(new ScoreIterationListener(500));
-        for (int i = 0; i < epochs; i++) {
-            model.fit(split.getTrain());
+        KFoldIterator kiter = new KFoldIterator(4, ds);
+        int k = 0;
+        double totalAccuracy = 0D;
+        while (kiter.hasNext()) {
+            final DataSet next = kiter.next();
+            for (int i = 0; i < epochs; i++) {
+                model.fit(next);
+            }
+            DataSet test = kiter.testFold();
+            //evaluate the model on the test set
+            Evaluation eval = new Evaluation(out);
+            INDArray output = model.output(test.getFeatures());
+            eval.eval(test.getLabels(), output);
+            log.info(eval.stats());
+            totalAccuracy += eval.accuracy();
+            k++;
         }
-        //evaluate the model on the test set
-        Evaluation eval = new Evaluation(out);
-        INDArray output = model.output(split.getTest().getFeatures());
-        eval.eval(split.getTest().getLabels(), output);
-        log.info(eval.stats());
+        totalAccuracy /= k;
+        System.out.println("Total Accuracy : " + totalAccuracy);
     }
 
     public static List<IUpdater> allUpdaters() {
@@ -165,10 +171,10 @@ public class Students {
         //            iUpdaters.add(new Adam(first));
         //            first /= 2;
         //        }
-        iUpdaters.add(new Adam()); // 62
+        iUpdaters.add(new Adam(1e-5)); // 62
         //   iUpdaters.add(new AdaMax()); //56
         //   iUpdaters.add(new AMSGrad()); //62
-        iUpdaters.add(new Nadam()); //60
+      //  iUpdaters.add(new Nadam()); //60
         //     iUpdaters.add(new Nesterovs()); //56
         //    iUpdaters.add(new NoOp()); //52
         //     iUpdaters.add(new RmsProp()); //51
@@ -178,7 +184,7 @@ public class Students {
 
     public static List<LossFunctions.LossFunction> allLossFuncs() {
         final List<LossFunctions.LossFunction> list = new ArrayList<>();
-        list.add(LossFunctions.LossFunction.MSE); //59
+     //   list.add(LossFunctions.LossFunction.MSE); //59
         //  list.add(LossFunctions.LossFunction.L1);//60
         //  list.add(LossFunctions.LossFunction.MCXENT);//60
         //  list.add(LossFunctions.LossFunction.KL_DIVERGENCE);//60
